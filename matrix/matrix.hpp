bca5cde7eb5faf1feb8e8be491249bfad44fb708
h4x4d <sidenkoleg@gmail.com>
2023-11-08 23:37:13 +0300

    matrix fixes

--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -6,58 +6,22 @@ class Matrix {
  public:
   Matrix() : Matrix(T()){};
 
-  Matrix(const std::vector<std::vector<T>>& vector)
-      : matrix_(std::vector<std::vector<T>>(N, std::vector<T>(M))) {
-    for (size_t i = 0; i < N; ++i) {
-      for (size_t j = 0; j < M; ++j) {
-        matrix_[i][j] = vector[i][j];
-      }
-    }
-  };
+  Matrix(const std::vector<std::vector<T>>& vector) : matrix_(vector){};
 
   Matrix(const Matrix<N, M, T>& matrix) : Matrix(matrix.matrix_){};
 
-  Matrix(const T& elem) {
-    matrix_ = std::vector<std::vector<T>>(N, std::vector<T>(M, elem));
-  };
+  Matrix(const T& elem)
+      : matrix_(std::vector<std::vector<T>>(N, std::vector<T>(M, elem))){};
 
-  Matrix& operator+=(const Matrix& other) {
-    for (size_t i = 0; i < N; ++i) {
-      for (size_t j = 0; j < M; ++j) {
-        matrix_[i][j] += other(i, j);
-      }
-    }
-    return *this;
-  };
+  Matrix& operator+=(const Matrix& other);
 
-  Matrix& operator-=(const Matrix& other) {
-    for (size_t i = 0; i < N; ++i) {
-      for (size_t j = 0; j < M; ++j) {
-        matrix_[i][j] -= other(i, j);
-      }
-    }
-    return *this;
-  };
+  Matrix& operator-=(const Matrix& other);
 
-  Matrix& operator+=(T element) {
-    for (size_t i = 0; i < N; ++i) {
-      for (size_t j = 0; j < M; ++j) {
-        matrix_[i][j] += element;
-      }
-    }
-    return *this;
-  };
+  Matrix& operator+=(T element);
 
   Matrix& operator-=(T element) { return (*this += (-element)); };
 
-  Matrix& operator*=(T element) {
-    for (size_t i = 0; i < N; ++i) {
-      for (size_t j = 0; j < M; ++j) {
-        matrix_[i][j] *= element;
-      }
-    }
-    return *this;
-  };
+  Matrix& operator*=(T element);
 
   T& operator()(size_t row, size_t col) { return matrix_[row][col]; };
 
@@ -65,24 +29,9 @@ class Matrix {
     return matrix_[row][col];
   };
 
-  Matrix<M, N, T> Transposed() {
-    Matrix<M, N, T> result;
-    for (size_t i = 0; i < N; ++i) {
-      for (size_t j = 0; j < M; ++j) {
-        result(j, i) = matrix_[i][j];
-      }
-    }
-    return result;
-  };
+  Matrix<M, N, T> Transposed();
 
-  T Trace() {
-    static_assert(N == M);
-    T result = 0;
-    for (size_t i = 0; i < N; ++i) {
-      result += matrix_[i][i];
-    }
-    return result;
-  };
+  T Trace();
 
  private:
   std::vector<std::vector<T>> matrix_;
@@ -138,3 +87,64 @@ Matrix<N, K, T> operator*(const Matrix<N, M, T>& first,
   }
   return result;
 }
+
+template <size_t N, size_t M, typename T>
+Matrix<N, M, T>& Matrix<N, M, T>::operator+=(const Matrix<N, M, T>& other) {
+  for (size_t i = 0; i < N; ++i) {
+    for (size_t j = 0; j < M; ++j) {
+      matrix_[i][j] += other(i, j);
+    }
+  }
+  return *this;
+};
+
+template <size_t N, size_t M, typename T>
+Matrix<N, M, T>& Matrix<N, M, T>::operator-=(const Matrix<N, M, T>& other) {
+  for (size_t i = 0; i < N; ++i) {
+    for (size_t j = 0; j < M; ++j) {
+      matrix_[i][j] -= other(i, j);
+    }
+  }
+  return *this;
+};
+
+template <size_t N, size_t M, typename T>
+Matrix<N, M, T>& Matrix<N, M, T>::operator+=(T element) {
+  for (size_t i = 0; i < N; ++i) {
+    for (size_t j = 0; j < M; ++j) {
+      matrix_[i][j] += element;
+    }
+  }
+  return *this;
+};
+
+template <size_t N, size_t M, typename T>
+Matrix<N, M, T>& Matrix<N, M, T>::operator*=(T element) {
+  for (size_t i = 0; i < N; ++i) {
+    for (size_t j = 0; j < M; ++j) {
+      matrix_[i][j] *= element;
+    }
+  }
+  return *this;
+};
+
+template <size_t N, size_t M, typename T>
+Matrix<M, N, T> Matrix<N, M, T>::Transposed() {
+  Matrix<M, N, T> result;
+  for (size_t i = 0; i < N; ++i) {
+    for (size_t j = 0; j < M; ++j) {
+      result(j, i) = matrix_[i][j];
+    }
+  }
+  return result;
+};
+
+template <size_t N, size_t M, typename T>
+T Matrix<N, M, T>::Trace() {
+  static_assert(N == M);
+  T result = 0;
+  for (size_t i = 0; i < N; ++i) {
+    result += matrix_[i][i];
+  }
+  return result;
+};
