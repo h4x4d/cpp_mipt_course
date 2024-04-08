b3a6944e2a7e5e3dd7bd820589026e5e2200cf47
h4x4d <sidenkoleg@gmail.com>
2024-04-08 15:12:58 +0300

    first review fixes

--- deque_pt2/deque_pt2.hpp
+++ deque_pt2/deque_pt2.hpp
@@ -99,6 +99,12 @@ class Deque {
   ~Deque() { clear_buckets(); }
 
  private:
+  allocator_type allocator_;
+  using allocator_traits = std::allocator_traits<Allocator>;
+  using bucket_allocator_type =
+      typename allocator_traits::template rebind_alloc<T*>;
+  using bucket_traits = std::allocator_traits<bucket_allocator_type>;
+
   constexpr static size_t kBucketSize = 8;
   using bucket = T*;
 
@@ -112,7 +118,8 @@ class Deque {
     return init_any_bucket(bucket_index, buckets_, allocator_);
   };
 
-  void init_any_bucket(size_t bucket_index, std::vector<bucket>& buckets,
+  void init_any_bucket(size_t bucket_index,
+                       std::vector<bucket, bucket_allocator_type>& buckets,
                        Allocator& allocator) const;
 
   size_t get_absolute(size_t index) const {
@@ -120,13 +127,10 @@ class Deque {
            (kBucketSize * buckets_amount_);
   };
 
-  std::vector<bucket> buckets_;
-  size_t buckets_amount_ = 0;
   size_t size_ = 0;
   size_t start_shift_ = 0;
-
-  allocator_type allocator_;
-  using allocator_traits = std::allocator_traits<Allocator>;
+  size_t buckets_amount_ = 0;
+  std::vector<bucket, bucket_allocator_type> buckets_;
 };
 
 template <typename T, typename Allocator>
@@ -134,18 +138,18 @@ Deque<T, Allocator>::Deque(size_t count, const Allocator& allocator)
     : allocator_(allocator) {
   buckets_amount_ = std::ceil(count / static_cast<double>(kBucketSize));
   buckets_.resize(buckets_amount_);
-  for (size_t i = 0; i < buckets_amount_; ++i) {
-    try {
+  try {
+    for (size_t i = 0; i < buckets_amount_; ++i) {
       init_bucket(i);
       for (size_t j = 0; j < std::min(count, kBucketSize); ++j) {
         allocator_traits::construct(allocator_, buckets_[i] + j);
         size_++;
       }
       count -= std::min(count, kBucketSize);
-    } catch (...) {
-      clear_buckets();
-      throw;
     }
+  } catch (...) {
+    clear_buckets();
+    throw;
   }
 }
 
@@ -154,7 +158,7 @@ Deque<T, Allocator>::Deque(std::initializer_list<T> init,
                            const Allocator& allocator)
     : allocator_(allocator) {
   for (auto iter = init.begin(); iter != init.end(); ++iter) {
-    push_back(*iter);
+    emplace_back(std::forward<decltype(*iter)>(*iter));
   }
 }
 
@@ -173,7 +177,7 @@ Deque<T, Allocator>::Deque(Deque&& other)
 
 template <typename T, typename Allocator>
 void Deque<T, Allocator>::pop_back() {
-  operator[](size() - 1).~T();
+  allocator_traits::destroy(allocator_, &operator[](size() - 1));
   --size_;
 }
 
@@ -188,7 +192,7 @@ void Deque<T, Allocator>::erase(Deque::iterator iter) {
 template <typename T, typename Allocator>
 void Deque<T, Allocator>::insert(Deque::iterator iter, const T& value) {
   T last_value = value;
-  while (iter != end()) {
+  for (; iter != end(); iter++) {
     std::swap(last_value, *iter);
     iter++;
   }
@@ -197,78 +201,29 @@ void Deque<T, Allocator>::insert(Deque::iterator iter, const T& value) {
 
 template <typename T, typename Allocator>
 void Deque<T, Allocator>::pop_front() {
-  operator[](start_shift_).~T();
-  start_shift_ = (start_shift_ + 1) % (kBucketSize * buckets_amount_);
+  allocator_traits::destroy(allocator_, &operator[](start_shift_));
+  (++start_shift_) %= (kBucketSize * buckets_amount_);
   --size_;
 }
 
 template <typename T, typename Allocator>
 void Deque<T, Allocator>::push_front(T&& value) {
-  if (size_ == kBucketSize * buckets_amount_) {
-    reallocate_buckets();
-  }
-
-  size_t absolute_index = get_absolute(-1);
-  size_t bucket = absolute_index / kBucketSize;
-
-  if (buckets_[bucket] == nullptr) {
-    init_bucket(bucket);
-  }
-  allocator_traits::construct(allocator_, &operator[](-1), std::move(value));
-  start_shift_ = absolute_index;
-  size_++;
+  emplace_front(std::move(value));
 }
 
 template <typename T, typename Allocator>
 void Deque<T, Allocator>::push_front(const T& value) {
-  if (size_ == kBucketSize * buckets_amount_) {
-    reallocate_buckets();
-  }
-
-  size_t absolute_index = get_absolute(-1);
-  size_t bucket = absolute_index / kBucketSize;
-
-  if (buckets_[bucket] == nullptr) {
-    init_bucket(bucket);
-  }
-  allocator_traits::construct(allocator_, &operator[](-1), value);
-  start_shift_ = absolute_index;
-  size_++;
+  emplace_front(value);
 }
 
 template <typename T, typename Allocator>
 void Deque<T, Allocator>::push_back(T&& value) {
-  if (size_ >= kBucketSize * buckets_amount_) {
-    reallocate_buckets();
-  }
-
-  size_t absolute_index = get_absolute(size_);
-  size_t bucket = absolute_index / kBucketSize;
-
-  if (buckets_[bucket] == nullptr) {
-    init_bucket(bucket);
-  }
-
-  allocator_traits::construct(allocator_, &operator[](size()),
-                              std::move(value));
-  size_++;
+  emplace_back(std::move(value));
 }
 
 template <typename T, typename Allocator>
 void Deque<T, Allocator>::push_back(const T& value) {
-  if (size_ >= kBucketSize * buckets_amount_) {
-    reallocate_buckets();
-  }
-
-  size_t absolute_index = get_absolute(size_);
-  size_t bucket = absolute_index / kBucketSize;
-
-  if (buckets_[bucket] == nullptr) {
-    init_bucket(bucket);
-  }
-
-  allocator_traits::construct(allocator_, &operator[](size()), value);
-  size_++;
+  emplace_back(value);
 }
 
 template <typename T, typename Allocator>
@@ -278,15 +233,15 @@ void Deque<T, Allocator>::emplace_back(Arguments&&... args) {
     reallocate_buckets();
   }
 
-  size_t absolute_index = get_absolute(-1);
+  size_t absolute_index = get_absolute(size_);
   size_t bucket = absolute_index / kBucketSize;
 
   if (buckets_[bucket] == nullptr) {
     init_bucket(bucket);
   }
-  allocator_traits::construct(allocator_, &operator[](-1),
+
+  allocator_traits::construct(allocator_, &operator[](size_),
                               std::forward<Arguments>(args)...);
-  start_shift_ = absolute_index;
   size_++;
 }
 
@@ -297,15 +252,16 @@ void Deque<T, Allocator>::emplace_front(Arguments&&... args) {
     reallocate_buckets();
   }
 
-  size_t absolute_index = get_absolute(size_);
+  size_t absolute_index = get_absolute(-1);
   size_t bucket = absolute_index / kBucketSize;
 
   if (buckets_[bucket] == nullptr) {
     init_bucket(bucket);
   }
 
-  allocator_traits::construct(allocator_, &operator[](size()),
+  allocator_traits::construct(allocator_, &operator[](-1),
                               std::forward<Arguments>(args)...);
+  start_shift_ = absolute_index;
   size_++;
 }
 
@@ -316,7 +272,8 @@ void Deque<T, Allocator>::reallocate_buckets() {
     big_buckets_amount = 1;
   }
 
-  std::vector<bucket> created_buckets(big_buckets_amount, nullptr);
+  std::vector<bucket, bucket_allocator_type> created_buckets(
+      big_buckets_amount, nullptr, allocator_);
   size_t now = 0;
   size_t start_bucket = start_shift_ / kBucketSize;
   if (start_shift_ % kBucketSize != 0 &&
@@ -327,7 +284,7 @@ void Deque<T, Allocator>::reallocate_buckets() {
       for (; local_ind < kBucketSize; ++local_ind) {
         allocator_traits::construct(
             allocator_, created_buckets[now] + local_ind,
-            std::move(*(buckets_[start_bucket] + local_ind)));
+            std::move(buckets_[start_bucket][local_ind]));
       }
     } catch (...) {
       for (size_t j = start_shift_ % kBucketSize; j < local_ind; ++j) {
@@ -352,13 +309,13 @@ void Deque<T, Allocator>::reallocate_buckets() {
     created_buckets[now++] = buckets_[i];
   }
   buckets_amount_ = big_buckets_amount;
-  buckets_ = created_buckets;
+  buckets_ = std::move(created_buckets);
 }
 
 template <typename T, typename Allocator>
-void Deque<T, Allocator>::init_any_bucket(size_t bucket_index,
-                                          std::vector<bucket>& buckets,
-                                          Allocator& allocator) const {
+void Deque<T, Allocator>::init_any_bucket(
+    size_t bucket_index, std::vector<bucket, bucket_allocator_type>& buckets,
+    Allocator& allocator) const {
   if (buckets[bucket_index] == nullptr) {
     buckets[bucket_index] = allocator_traits::allocate(allocator, kBucketSize);
   }
@@ -388,8 +345,7 @@ void Deque<T, Allocator>::swap(Deque& other) {
 
 template <typename T, typename Allocator>
 Deque<T, Allocator>& Deque<T, Allocator>::operator=(Deque&& other) {
-  Deque<T, Allocator> tmp = std::move(other);
-  swap(tmp);
+  swap(std::move(other));
   return *this;
 }
 
@@ -398,12 +354,14 @@ Deque<T, Allocator>& Deque<T, Allocator>::operator=(const Deque& other) {
   if ((this) == &other) {
     return *this;
   }
-  std::vector<bucket> replace_buckets(other.buckets_amount_);
 
   auto tmp_allocator = allocator_;
   if (allocator_traits::propagate_on_container_copy_assignment::value) {
     tmp_allocator = other.allocator_;
   }
+  std::vector<bucket, typename std::allocator_traits<
+                          decltype(tmp_allocator)>::template rebind_alloc<T*>>
+      replace_buckets(other.buckets_amount_, tmp_allocator);
   size_t left_to_copy = other.size_;
   size_t shift = other.start_shift_;
   size_t created_now = 0;
@@ -437,7 +395,7 @@ Deque<T, Allocator>& Deque<T, Allocator>::operator=(const Deque& other) {
   }
   clear_buckets();
 
-  buckets_ = replace_buckets;
+  buckets_ = std::move(replace_buckets);
   size_ = other.size_;
   start_shift_ = other.start_shift_;
   buckets_amount_ = other.buckets_amount_;
@@ -457,10 +415,10 @@ Deque<T, Allocator>::Deque(const Deque& other)
 
   size_t left_to_copy = size_;
   size_t shift = start_shift_;
-  for (size_t i = 0; i < buckets_amount_; ++i) {
-    if (other.buckets_[i] != nullptr) {
-      init_bucket(i);
-      try {
+  try {
+    for (size_t i = 0; i < buckets_amount_; ++i) {
+      if (other.buckets_[i] != nullptr) {
+        init_bucket(i);
         for (size_t j = shift; j < std::min(shift + left_to_copy, kBucketSize);
              ++j) {
           allocator_traits::construct(allocator_, buckets_[i] + j,
@@ -468,32 +426,34 @@ Deque<T, Allocator>::Deque(const Deque& other)
         }
         shift = 0;
         left_to_copy -= kBucketSize;
-      } catch (...) {
-        clear_buckets();
+
+      } else {
+        buckets_[i] = nullptr;
       }
-    } else {
-      buckets_[i] = nullptr;
     }
+  } catch (...) {
+    clear_buckets();
   }
 }
 
 template <typename T, typename Allocator>
 Deque<T, Allocator>::Deque(size_t count, const T& value,
                            const Allocator& allocator)
-    : size_(count), allocator_(allocator) {
-  buckets_amount_ = std::ceil(size_ / static_cast<double>(kBucketSize));
-  buckets_.resize(buckets_amount_);
-  for (size_t i = 0; i < buckets_amount_; ++i) {
-    try {
+    : size_(count),
+      allocator_(allocator),
+      buckets_amount_(std::ceil(size_ / static_cast<double>(kBucketSize))),
+      buckets_(buckets_amount_, nullptr, allocator_) {
+  try {
+    for (size_t i = 0; i < buckets_amount_; ++i) {
       init_bucket(i);
       for (size_t j = 0; j < std::min(count, kBucketSize); ++j) {
         allocator_traits::construct(allocator_, buckets_[i] + j, value);
       }
       count -= std::min(count, kBucketSize);
-    } catch (...) {
-      clear_buckets();
-      throw;
     }
+  } catch (...) {
+    clear_buckets();
+    throw;
   }
 }
 
