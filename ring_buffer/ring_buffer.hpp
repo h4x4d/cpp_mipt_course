bcfb088a7657737b3006ecfe43e18eb69d307291
h4x4d <sidenkoleg@gmail.com>
2024-10-29 10:58:36 +0300

    fix ring_buffer

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -1,34 +1,41 @@
 #include <cstddef>
-#include <vector>
 
+template <typename T>
 class RingBuffer {
  public:
-  explicit RingBuffer(size_t capacity) : kCapacity(capacity) {
-    queue_.reserve(kCapacity);
-  }
+  explicit RingBuffer(size_t capacity)
+      : buffer_(new T[capacity]), kCapacity(capacity) {}
 
-  size_t Size() const { return queue_.size(); }
+  size_t Size() const { return size_; }
 
-  bool Empty() const { return queue_.empty(); }
+  bool Empty() const { return size_ == 0; }
 
-  bool TryPush(int element) {
-    if (queue_.size() < kCapacity) {
-      queue_.push_back(element);
+  bool TryPush(const T& element) {
+    if (size_ < kCapacity) {
+      buffer_[(shift_ + size_) % kCapacity] = element;
+      ++size_;
       return true;
     }
     return false;
   }
 
-  bool TryPop(int* element) {
-    if (!queue_.empty()) {
-      *element = queue_[0];
-      queue_.erase(queue_.begin());
+  bool TryPop(T* element) {
+    if (!Empty()) {
+      *element = buffer_[shift_];
+      shift_ = (shift_ + 1) % kCapacity;
+      --size_;
       return true;
     }
     return false;
+  }
+
+  ~RingBuffer() {
+    delete[] buffer_;
   }
 
  private:
-  std::vector<int> queue_;
+  T* buffer_;
+  size_t shift_ = 0;
+  size_t size_ = 0;
   const size_t kCapacity;
 };
